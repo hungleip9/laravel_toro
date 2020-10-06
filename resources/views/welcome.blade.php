78002b016f324f3f1147c0caad20be3f98dec80d
hungleip9 <66316466+hungleip9@users.noreply.github.com>
2020-10-06 10:56:19 +0700

    buoi 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,13 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Bai 1</title>
 </head>
 <body>
-<ul>
-    <li>Họ và tên: Lê Quang Hùng</li>
-    <li>Năm sinh: 1998 </li>
-    <li>Trường học đang/đã theo học: Zent</li>
-    <li>Mục tiêu sau khi học xong khoá học Laravel: Đi làm</li>
-</ul>
+<form action="{{route('save')}}" method="POST">
+
+    @csrf()
+    <button> SubMit </button>
+</form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,103 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+////xoa
+//Route::delete('/task/delete', function () {
+//    dd('delete');
+//});
+//// xoa xong chuyen huong
+//Route::delete('/task/delete', function () {
+//    return redirect('/');
+//});
+
+// cach chuyen tham so id
+Route::get('user/{id?}', function($id = null) {
+    echo route('trump');
+    return 'User ' . $id;
+});
+Route::get('u/{id?}', function($id = null) {
+    echo route('trump');
+    return ' User ' . $id;
+});
+
+Route::get('/user/{id}/post/{post}', function($id, $idPost) {
+    return "This is post $idPost of user $id";
+});
+
+
+Route::view('/welcome', 'welcome');
+Route::get('foo', function () {
+    return 'hello world';
+});
+Route::get('task/list/hung/hung/hung/hung', function () {
+    return 'hello world';
+});
+//class-room/LARAVEL2020004/units
+Route::get('class-room/LARAVEL2020004/units', function () {
+    return 'hello world';
+});
+//nobel-y-sinh-cho-ba-nha-khoa-hoc-phat-hien-virus-viem-gan-c-4171906.html
+Route::get('2020-nobel-y-sinh-cho-ba-nha-khoa-hoc-phat-hien-virus-viem-gan-c-4171906.html', function () {
+    return 'nobel-y-sinh-cho-ba-nha-khoa-hoc-phat-hien-virus-viem-gan-c-4171906.html';
+})->name('trump');
+
+Route::post('save',function (){
+   echo 'post';
+})->name('save');
+
+//Route::get('save',function (){
+//   echo 'post';
+//});
+//Route::match(['get','post'], 'save',function (){
+//    echo route('trump');
+//});
+Route::prefix('admin')->group(function (){
+    Route::prefix('user')->group(function (){
+        Route::get('edit',function (){
+            echo 'admin/user/edit';
+        });
+        Route::get('delete',function (){
+            echo 'admin/user/delete';
+        });
+        Route::get('detail',function (){
+            echo 'admin/user/detail';
+        });
+    });
+    Route::get('edit',function (){
+        echo 'admin/edit';
+    });
+    Route::get('post',function (){
+        echo 'admin/post';
+    });
+    Route::get('delete',function (){
+        echo 'admin/delete';
+    });
+});
+// cach 2
+//Route::group(
+//    [
+//        'prefix' => 'user'
+//    ]
+//    ,function (){
+//    Route::get('edit',function (){
+//        echo 'admin/user/edit';
+//    });
+//    Route::get('delete',function (){
+//        echo 'admin/user/delete';
+//    });
+//    Route::get('detail',function (){
+//        echo 'admin/user/detail';
+//    });
+//});
+
+//tinh  nang hoan thanh
+Route::prefix('task')->group(function (){
+    Route::get('complete/3',function (){
+        echo 'hoan thanh';
+    })->name('todo.task.complete');
+//tinh nang lam lai
+    Route::get('reset/3',function (){
+        echo 'Lam lai';
+    })->name('todo.task.reset');
+});
+
